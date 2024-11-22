06d2f807b2b9f4908bff146fd943af30e4908042
albertoig <alberto@iglesias.cloud>
2024-11-22 18:09:06 -0500

    fix(actions): Fix version manually 0.0.2

--- certification/3-servicing-and-networking/practices/code/3-frontend/src/app/page.tsx
+++ certification/3-servicing-and-networking/practices/code/3-frontend/src/app/page.tsx
@@ -1,26 +1,35 @@
-import { useState, useEffect } from 'react';
 
-export default function Page() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
+import { headers } from 'next/headers';
 
-  useEffect(() => {
-    const callAPI = async () => {
-      try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/`);
-        const data = await res.json();
-        setData(data);
-      } catch (err) {
-        setError(err.message);
-      }
-    };
+async function fetchData() {
+	const headersInstance = await headers();
+	// Now you can use headersInstance safely
+	const res = await fetch(`${process.env.BACKEND_URL}/`, {
+		headers: headersInstance,
+	});
+	if (!res.ok) {
+		throw new Error('Data fetch failed');
+	}
+	return res.json();
+}
 
-    callAPI();
-  }, []);
+export default async function Page() {
+  let data;
+  let error;
+
+  try {
+    data = await fetchData();
+  } catch (err) {
+    error = err.message;
+    // Server-side logging
+    console.error('Server-side error:', err);
+  }
 
   if (error) return <p>Caught an error: {error}</p>;
 
-  if (!data) return <p>Loading...</p>;
-
-  return <p>Hello, This is the Frontend Practice of the CKA exam! The backend call result is: {JSON.stringify(data)}</p>;
+  return (
+    <div>
+      <p>Hello, This is the Frontend Practice of the CKA exam! The backend call result is: {JSON.stringify(data)}</p>
+    </div>
+  );
 }
